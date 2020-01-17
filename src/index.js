58c99cf8668577efea6afe95e6329c91e4fd7217
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2020-01-17 18:21:19 -0500

    Move links to another component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
 import App from './app';
 import Users from './users';
 import Notes from './notes';
@@ -11,17 +11,6 @@ import * as serviceWorker from './serviceWorker';
 const routing = (
     <Router>
         <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/notes">Notes</Link>
-                </li>
-                <li>
-                    <Link to="/Users">Users</Link>
-                </li>
-            </ul>
             <Route exact path="/" component={App} />
             <Route path="/users" component={Users} />
             <Route path="/notes" component={Notes} />

85a7b8d3b77abb1ab65dc0150684f684673344e9
Lubomir Stankov <lubomir.c.stankov@gmail.com>
2020-01-17 18:14:32 -0500

    Adding default routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import App from './app';
+import Users from './users';
+import Notes from './notes';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/notes">Notes</Link>
+                </li>
+                <li>
+                    <Link to="/Users">Users</Link>
+                </li>
+            </ul>
+            <Route exact path="/" component={App} />
+            <Route path="/users" component={Users} />
+            <Route path="/notes" component={Notes} />
+        </div>
+    </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(routing, document.getElementById('root'));
+
+
 serviceWorker.unregister();
